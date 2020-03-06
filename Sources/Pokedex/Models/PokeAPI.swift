1235a6748d2932952fc26023914dd2b0d3ad21ba
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-06 16:28:25 +0200

    done cache

--- Sources/Pokedex/Models/PokeAPI.swift
+++ Sources/Pokedex/Models/PokeAPI.swift
@@ -4,46 +4,58 @@ import Vapor
 
 /// A simple wrapper around the "pokeapi.co" API.
 public final class PokeAPI {
-  /// The HTTP client powering this API.
-  let client: Client
-  
-  /// Creates a new `PokeAPI` wrapper from the supplied client and cache.
-  public init(client: Client) {
-    self.client = client
-  }
-  
-  /// Returns `true` if the supplied Pokemon name is real.
-  ///
-  /// - parameter client: Queries the "pokeapi.co" API to verify supplied names
-  /// - parameter cache: Caches client results to minimize slow, external API calls
-  public func verifyName(_ name: String, on worker: Worker) throws -> Future<Bool> {
-    /// Query the PokeAPI.
-    return self.fetchPokemon(named: name).map { res in
-      switch res.http.status.code {
-      case 200..<300:
-        /// The API returned 2xx which means this is a real Pokemon name
-        return true
-      case 404:
-        /// The API returned a 404 meaning this Pokemon name was not found.
-        return false
-      default:
-        /// The API returned a 500. Only thing we can do is forward the error.
-        throw Abort(.internalServerError, reason: "Unexpected PokeAPI response: \(res.http.status)")
-      }
+    /// The HTTP client powering this API.
+    let client: Client
+    let cache: KeyedCache
+    
+    /// Creates a new `PokeAPI` wrapper from the supplied client and cache.
+    public init(client: Client, cache: KeyedCache) {
+        self.client = client
+        self.cache = cache
     }
-  }
-  
-  /// Fetches a pokemen with the supplied name from the PokeAPI.
-  public func fetchPokemon(named name: String) -> Future<Response> {
-    return client.get("https://pokeapi.co/api/v2/pokemon/\(name)")
-  }
+    
+    /// Returns `true` if the supplied Pokemon name is real.
+    ///
+    /// - parameter client: Queries the "pokeapi.co" API to verify supplied names
+    /// - parameter cache: Caches client results to minimize slow, external API calls
+    public func verifyName(_ name: String, on worker: Worker) throws -> Future<Bool> {
+        let key = name.lowercased()
+        return cache.get(key, as: Bool.self).flatMap{ result in
+            if let exists = result {
+                return worker.eventLoop.newSucceededFuture(result: exists)
+            }
+            /// Query the PokeAPI.
+            return self.fetchPokemon(named: name).flatMap { res in
+                switch res.http.status.code {
+                case 200..<300:
+                    /// The API returned 2xx which means this is a real Pokemon name
+                    return self.cache.set(key, to: true).transform(to: true)
+                case 404:
+                    /// The API returned a 404 meaning this Pokemon name was not found.
+                    
+                    return self.cache.set(key, to: false).transform(to: false)
+                default:
+                    /// The API returned a 500. Only thing we can do is forward the error.
+                    let reason = "Unexpected PokeAPI response: \(res.http.status)"
+                    throw Abort(.internalServerError, reason: reason)
+                }
+            }
+        }
+        
+        
+    }
+    
+    /// Fetches a pokemen with the supplied name from the PokeAPI.
+    public func fetchPokemon(named name: String) -> Future<Response> {
+        return client.get("https://pokeapi.co/api/v2/pokemon/\(name)")
+    }
 }
 
 /// Allow our custom PokeAPI wrapper to be used as a Vapor service.
 extension PokeAPI: ServiceType {
-  /// See `ServiceType.makeService(for:)`
-  public static func makeService(for container: Container) throws -> PokeAPI {
-    /// Use the container to create the Client services our PokeAPI wrapper needs.
-    return try PokeAPI(client: container.make())
-  }
+    /// See `ServiceType.makeService(for:)`
+    public static func makeService(for container: Container) throws -> PokeAPI {
+        /// Use the container to create the Client services our PokeAPI wrapper needs.
+        return try PokeAPI(client: container.make(), cache: container.make())
+    }
 }
--- Sources/Pokedex/configure.swift
+++ Sources/Pokedex/configure.swift
@@ -5,35 +5,38 @@ import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-  /// Register providers
-  try services.register(FluentSQLiteProvider())
-  
-  /// Register routes to the router
-  services.register(Router.self) { c -> EngineRouter in
-    let router = EngineRouter.default()
-    try routes(router)
-    return router
-  }
-  
-  /// Register our custom PokeAPI wrapper
-  services.register(PokeAPI.self)
+    /// Register providers
+    try services.register(FluentSQLiteProvider())
     
-  /// Setup a simple in-memory SQLite database
-  let sqlite = try SQLiteDatabase(storage: .memory)
-  services.register(sqlite)
-  
-  /// Configure SQLite database
-  services.register { c -> DatabasesConfig in
-    var databases = DatabasesConfig()
-    try databases.add(database: c.make(SQLiteDatabase.self), as: .sqlite)
-    return databases
-  }
-  
-  /// Configure migrations
-  services.register { c -> MigrationConfig in
-    var migrations = MigrationConfig()
-    /// Ensure there is a table ready to store the Pokemon
-    migrations.add(model: Pokemon.self, database: .sqlite)
-    return migrations
-  }
+    /// Register routes to the router
+    services.register(Router.self) { c -> EngineRouter in
+        let router = EngineRouter.default()
+        try routes(router)
+        return router
+    }
+    
+    /// Register our custom PokeAPI wrapper
+    services.register(PokeAPI.self)
+    
+    /// Setup a simple in-memory SQLite database
+    let sqlite = try SQLiteDatabase(storage: .memory)
+    services.register(sqlite)
+    
+    /// Configure SQLite database
+    services.register { c -> DatabasesConfig in
+        var databases = DatabasesConfig()
+        try databases.add(database: c.make(SQLiteDatabase.self), as: .sqlite)
+        return databases
+    }
+    
+    /// Configure migrations
+    services.register { c -> MigrationConfig in
+        var migrations = MigrationConfig()
+        /// Ensure there is a table ready to store the Pokemon
+        migrations.add(model: Pokemon.self, database: .sqlite)
+        migrations.prepareCache(for: .sqlite)
+        return migrations
+    }
+    
+    config.prefer(SQLiteCache.self, for: KeyedCache.self)
 }
